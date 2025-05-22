2edbeff713ca0fddb9828945c6f1e9cd25206f94
Alex Malakov <alexander.malakov@gmail.com>
2025-05-21 21:54:07 -0400

    conditional move action abstraction

---
+++ Chess Project/Assets/Scripts/Enemies/EnemyActions/ConditionalMoveAction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ConditionalMoveAction : HostileEntityAction
+{
+    public override void act() {
+        Square bestSq = this.opponent.position;
+        int best = -1;
+        foreach(Square s in this.opponentMoves()) {
+            if(this.moveCondition(s) > best || (this.moveCondition(s) == best && Random.Range(0,2) == 0)) {
+                best = this.moveCondition(s);
+                bestSq = s;
+            }
+        }
+
+        yield return this.opponent.slide(bestSq);
+    }
+
+    public abstract List<Square> opponentMoves();
+
+    public abstract int moveCondition(Square s);
+}
