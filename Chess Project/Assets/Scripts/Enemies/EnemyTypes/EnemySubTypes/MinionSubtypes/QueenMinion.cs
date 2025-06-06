f295c748d64630b2798f759088774592b141bcbc
Alex Malakov <alexander.malakov@gmail.com>
2025-06-06 13:57:40 -0400

    refactor complete

--- Chess Project/Assets/Scripts/Enemies/EnemyTypes/EnemySubTypes/MinionSubtypes/QueenMinion.cs
+++ Chess Project/Assets/Scripts/Enemies/EnemyTypes/EnemySubTypes/MinionSubtypes/QueenMinion.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QueenMinion : MonoBehaviour
+public class QueenMinion : Minion
 {
-    // public override void initActions() {
-    //     this.actionQueue = new List<HostileEntityAction>();
-    //     this.actionLoop = new List<HostileEntityAction>();
-    // }
+    public override void initActions() {
+        this.actionQueue = new List<HostileEntityAction>();
+        this.actionLoop = new List<HostileEntityAction>();
+    }
 }
