c54b7d5d8e3b4217694953aef82ff8d39ae0a061
Alex Malakov <alexander.malakov@gmail.com>
2025-06-06 15:49:37 -0400

    effect manager

--- Chess Project/Assets/Scripts/Misc/UI/PopUpAndAnimation/CustomEffect.cs
+++ Chess Project/Assets/Scripts/Misc/UI/PopUpAndAnimation/CustomEffect.cs
@@ -6,4 +6,18 @@ public class CustomEffect : MonoBehaviour
 {
     public List<string> possibleEffects;
     public List<GameObject> childEffects;
+    bool active = false;
+
+    public IEnumerator activate(Entity target, string effect, float duration) {
+        active = true;
+        this.transform.position = target.transform.position;
+        childEffects[possibleEffects.IndexOf(effect)].SetActive(true);
+        yield return new WaitForSeconds(duration);
+        childEffects[possibleEffects.IndexOf(effect)].SetActive(false);
+        active = false;
+    }
+
+    public bool isActive() {
+        return active;
+    }
 }
--- Chess Project/Assets/Scripts/Misc/UI/PopUpAndAnimation/EffectManager.cs
+++ Chess Project/Assets/Scripts/Misc/UI/PopUpAndAnimation/EffectManager.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//for now this will function similar to pop up manager, later it will manage animations probably
 public class EffectManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject effectObj;
+    List<CustomEffect> customEffectPool = new List<CustomEffect>();
+    int poolSize = 15;
+
+    public void Start() {
+        for(int i = 0; i < poolSize; i++) {
+            GameObject newEffect = Instantiate(effectObj);
+            this.customEffectPool.Add(newEffect.GetComponent<CustomEffect>());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public CustomEffect getNextEffect() {
+        foreach(CustomEffect c in this.customEffectPool) {
+            if(!c.isActive()) {
+                return c;
+            }
+        }
+
+        GameObject newEffect = Instantiate(effectObj);
+        CustomEffect effect = newEffect.GetComponent<CustomEffect>();
+        this.customEffectPool.Add(effect);
+        return effect;
     }
+
+    public IEnumerator displayEffect(Entity target, string effect, float duration) {
+        yield return getNextEffect().activate(target, effect, duration);
+    }
+
 }
